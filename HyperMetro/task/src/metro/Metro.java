6adfeef16cfa10d88bf3c825e09cb4e3d7389e04
Remigiusz <repogplus@gmail.com>
2022-02-14 21:49:06 +0100

    Stage completed

--- HyperMetro/task/src/metro/Metro.java
+++ HyperMetro/task/src/metro/Metro.java
@@ -1,59 +1,112 @@
 package metro;
 
-import java.io.File;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileReader;
 import java.io.IOException;
+
+import java.lang.reflect.Type;
 import java.util.*;
+
 
 class Line {
     private String name;
-    LinkedList<String> stationsList;
+    private Station station;
+    public LinkedList<Station> stationsList;
 
     public Line(String name) {
         this.name = name;
         this.stationsList = new LinkedList<>();
     }
 
-    public void setStationsList(LinkedList<String> stationsList) {
-        this.stationsList = stationsList;
+    public String getName() {
+        return name;
+    }
+
+    public Station getStation(String stationName) {
+        return stationsList.stream()
+                .filter(s -> s.getName().equals(stationName))
+                .findAny()
+                .orElse(null);
+    }
+}
+
+class Station {
+    private String name;
+    private List<Transfer> transfer;
+
+    public Station(String name) {
+        this.name = name;
+        this.transfer = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
+
+    public void setTransfer(Transfer transition) {
+        transfer.add(transition);
+    }
+
+    @Override
+    public String toString() {
+        if (transfer.size() != 0) {
+            return name + transfer.get(0);
+        } else return name;
+    }
+}
+
+class Transfer {
+    private String line;
+    private String station;
+
+    public Transfer(String line, String station) {
+        this.line = line;
+        this.station = station;
+    }
+
+    @Override
+    public String toString() {
+        return " - " + station + " (" + line + " line)";
+    }
 }
 
 public class Metro {
 
-    public Map<String, Map<Integer, String>> linesMap;
+    private Map<String, Map<Integer, Station>> linesMap;
     private List<Line> linesList;
 
     public Metro() {
         this.linesMap = new HashMap<>();
     }
 
-    public String readStationsFile(String filePath) {
-        StringBuilder jsonSerialized = new StringBuilder();
-        File file = new File(filePath);
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNext()) {
-                jsonSerialized.append(scanner.nextLine());
-            }
+    public void readStationsFile(String filePath) {
+        Type type = new TypeToken<HashMap<String, HashMap<Integer, Station>>>() {
+        }.getType();
+        Gson gson = new Gson();
+        try (JsonReader reader = new JsonReader(new FileReader(filePath))) {
+            linesMap = gson.fromJson(reader, type);
+        } catch (com.google.gson.JsonIOException | com.google.gson.JsonSyntaxException e) {
+            System.out.println("Incorrect file");
         } catch (IOException e) {
             System.out.println("Error! Such a file doesn't exist!");
         }
-        return jsonSerialized.toString();
+        mapAndSortMetroLines();
     }
 
-    public void mapDeserializedMetroLines() {
+    public void mapAndSortMetroLines() {
         linesList = new ArrayList<>();
-        for (var l : linesMap.entrySet()) {
-            Line line = new Line(l.getKey());
-            // get single line map<> and loop stations map
-            for (Map.Entry<Integer, String> s : linesMap.get(l.getKey()).entrySet()) {
-                line.stationsList.add(s.getValue());
-            }
+        linesMap.forEach((mLine, stations) -> {
+            //create line and set name
+            Line line = new Line(mLine);
+            //sort stations by key and replace in old map , treeMap is sorted
+            linesMap.put(mLine, new TreeMap<>(stations));
+            //add stations objects to LinkedList
+            stations.forEach((no, stationData) -> line.stationsList.add(stationData));
             linesList.add(line);
-        }
+        });
     }
 
     public Line getLine(String lineName) {
@@ -64,29 +117,29 @@ public class Metro {
     }
 
     public void addStationToLine(String lineName, String station) {
-        Line line = getLine(lineName);
-        line.stationsList.add(station);
+        getLine(lineName).stationsList.addLast(new Station(station));
     }
 
     public void addStationToLineHead(String lineName, String station) {
-        Line line = getLine(lineName);
-        line.stationsList.addFirst(station);
+        getLine(lineName).stationsList.addFirst(new Station(station));
     }
 
     public void removeStationFromLine(String lineName, String station) {
         Line line = getLine(lineName);
-        line.stationsList.remove(station);
+        line.stationsList.remove(line.getStation(station));
     }
-//    public void sortStations() {
-//        Map<Integer, String> stationsMap = linesMap.get()
-//    }
+
+    public void connectStations(String fromLine, String fromStation, String toLine, String toStation) {
+        getLine(fromLine).getStation(fromStation).setTransfer(new Transfer(toLine, toStation));
+        getLine(toLine).getStation(toStation).setTransfer(new Transfer(fromLine, toStation));
+    }
 
     public void printLine(String lineName) {
         Line line = getLine(lineName);
+        System.out.println("depot");
         for (int i = 0; i < line.stationsList.size(); i++) {
-            String previous = i == 0 ? "depot" : line.stationsList.get(i - 1);
-            String next = i == line.stationsList.size() - 1 ? "depot" : line.stationsList.get(i + 1);
-            System.out.println(previous + " - " + line.stationsList.get(i) + " - " + next);
+            System.out.println(line.stationsList.get(i));
         }
+        System.out.println("depot");
     }
 }
